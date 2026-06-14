0d6964024b2a1cc7120114d01a6e47d1b7ab55c9
Abdelrhman-yasser <yabdo5847@gmail.com>
2026-06-14 14:56:46 +0300

    Fix file paths for GitHub Pages deployment part 2

--- fix_paths.py
+++ fix_paths.py
@@ -1,9 +1,21 @@
 import glob
+import codecs
 
 html_files = glob.glob('*.html')
 for f in html_files:
-    with open(f, 'r', encoding='utf-8') as file:
-        content = file.read()
+    # Read with fallback encodings
+    content = None
+    for enc in ['utf-8', 'utf-16', 'windows-1252']:
+        try:
+            with open(f, 'r', encoding=enc) as file:
+                content = file.read()
+            break
+        except Exception:
+            pass
+            
+    if content is None:
+        print(f"Could not read {f}")
+        continue
     
     content = content.replace('href="style.css?v=2"', 'href="./src/css/style.css"')
     content = content.replace('href="style.css"', 'href="./src/css/style.css"')
@@ -16,7 +28,6 @@ for f in html_files:
     content = content.replace('<script src="app.js"></script>', '<script src="./src/js/app.js"></script>')
     content = content.replace('<script src="app.js?v=3"></script>', '<script src="./src/js/app.js"></script>')
 
-    # Fix image uploads paths
     content = content.replace('src="logo.png"', 'src="./logo.png"')
     
     with open(f, 'w', encoding='utf-8') as file:
