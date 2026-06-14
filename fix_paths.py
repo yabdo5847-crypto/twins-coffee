074daf1e04adcca9007180d53c120e9256e13f17
Abdelrhman-yasser <yabdo5847@gmail.com>
2026-06-14 14:55:36 +0300

    Fix file paths for GitHub Pages deployment

---
+++ fix_paths.py
@@ -0,0 +1,23 @@
+import glob
+
+html_files = glob.glob('*.html')
+for f in html_files:
+    with open(f, 'r', encoding='utf-8') as file:
+        content = file.read()
+    
+    content = content.replace('href="style.css?v=2"', 'href="./src/css/style.css"')
+    content = content.replace('href="style.css"', 'href="./src/css/style.css"')
+    content = content.replace('href="/src/css/style.css"', 'href="./src/css/style.css"')
+    
+    content = content.replace('<script src="api.js"></script>', '<script src="./src/js/api.js"></script>')
+    content = content.replace('<script src="api.js?v=4"></script>', '<script src="./src/js/api.js"></script>')
+    
+    content = content.replace('<script type="module" src="/src/js/app.js"></script>', '<script src="./src/js/app.js"></script>')
+    content = content.replace('<script src="app.js"></script>', '<script src="./src/js/app.js"></script>')
+    content = content.replace('<script src="app.js?v=3"></script>', '<script src="./src/js/app.js"></script>')
+
+    # Fix image uploads paths
+    content = content.replace('src="logo.png"', 'src="./logo.png"')
+    
+    with open(f, 'w', encoding='utf-8') as file:
+        file.write(content)
